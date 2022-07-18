700c1fecab006555fcbc77f79ecab16ecbbd5a6b
Kavindu Deshan <kkavindu.deshan@gmail.com>
2022-07-18 06:05:40 +0530

    state handling

--- src/components/Home/Countable/index.jsx
+++ src/components/Home/Countable/index.jsx
@@ -1,29 +1,41 @@
 import { Typography } from "@mui/material";
 import React, { Component } from "react";
-import Button from '@mui/material/Button';
+import Button from "@mui/material/Button";
 
+class Countable extends Component {
+  constructor(props) {
+    super(props);
 
-class Countable extends Component {    
-    constructor(props) {
-        super(props);
-    }
+    this.state = {
+      count: 0,
+    };
+  }
 
-    render() {
-        return (
-            <div>
-                <Typography variant="h4" gutterBottom>
-                    Count: 0
-                </Typography>
-                <Button
-                    variant="contained"
-                    onClick={() => {
-                        console.log('clicked!')
-                    }}
-                >
-                    Increase!
-                </Button>
-            </div>
-        )
-    }
+  incrementCount() {
+    console.log("increment function calling");
+    this.setState({
+      count: this.state.count + 1,
+    });
+  }
+
+  render() {
+    const count = 0;
+    return (
+      <div>
+        <Typography variant="h4" gutterBottom>
+          Count: {this.state.count}
+        </Typography>
+        <Button
+          variant="contained"
+          onClick={() => {
+            console.log("clicked!");
+            this.incrementCount();
+          }}
+        >
+          Increase!
+        </Button>
+      </div>
+    );
+  }
 }
 export default Countable;
