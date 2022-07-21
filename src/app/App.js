99237b5a613785ca7e07e09e93648d5f93254c7e
Kavindu Deshan <kkavindu.deshan@gmail.com>
2022-07-21 09:07:44 +0530

    Added Flex Layout

--- src/app/App.js
+++ src/app/App.js
@@ -2,14 +2,15 @@ import React from "react";
 import HomePage from "../pages/Home";
 import Login from "../pages/Session/Login";
 import NotFound from "../pages/Session/NotFound";
-import {Routes, Route} from "react-router-dom";
-
+import { Routes, Route } from "react-router-dom";
+import FlexBoxLayout from "../pages/Layouts/FlexBox";
 
 function App() {
   return (
     <Routes>
       <Route exact path="/" element={<HomePage />} />
       <Route path="login" element={<Login />} />
+      <Route path="flex-layout" element={<FlexBoxLayout />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
