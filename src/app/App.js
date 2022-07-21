e6944413c5564faf2feb4ba62e4b08d19cd6932c
Kavindu Deshan <kkavindu.deshan@gmail.com>
2022-07-21 16:46:07 +0530

    Route set for Posts

--- src/app/App.js
+++ src/app/App.js
@@ -5,11 +5,13 @@ import NotFound from "../pages/Session/NotFound";
 import { Routes, Route } from "react-router-dom";
 import FlexBoxLayout from "../pages/Layouts/FlexBox";
 import GridLayout from "../pages/Layouts/Grid";
+import Posts from "../../src/pages/posts";
 
 function App() {
   return (
     <Routes>
       <Route exact path="/" element={<HomePage />} />
+      <Route exact path="posts" element={<Posts />} />
       <Route path="login" element={<Login />} />
       <Route path="flex-layout" element={<FlexBoxLayout />} />
       <Route path="grid-layout" element={<GridLayout />} />
