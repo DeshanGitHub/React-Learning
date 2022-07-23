7f69d3c0e2fc9c3e346646ee663046384e717db0
Kavindu Deshan <kkavindu.deshan@gmail.com>
2022-07-23 11:57:02 +0530

    Added Save Customer

--- src/pages/Customers/index.jsx
+++ src/pages/Customers/index.jsx
@@ -1,105 +1,170 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import { Grid, Typography } from "@mui/material";
 import { TextValidator, ValidatorForm } from "react-material-ui-form-validator";
 import GDSEButton from "../../components/Common/Button";
+import CustomerService from "../../service/CustomerService";
+import GDSESnackBar from "../../components/Common/SnackBar";
 
 class Customer extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      formData: {
+        id: "",
+        name: "",
+        address: "",
+        salary: "",
+      },
+      alert: false,
+      message: "",
+      severity: "",
+    };
   }
+
+  clearFields = () => {
+    this.setState({
+      formData: {
+        id: "",
+        name: "",
+        address: "",
+        salary: "",
+      },
+    });
+  };
+
+  submitCustomer = async () => {
+    let formData = this.state.formData;
+    let res = await CustomerService.postCustomer(formData);
+
+    console.log(res); //print the promise
+
+    if (res.status === 201) {
+      this.setState({
+        alert: true,
+        message: res.data.message,
+        severity: "success",
+      });
+      this.clearFields();
+    } else {
+      this.setState({
+        alert: true,
+        message: res.response.data.message,
+        severity: "error",
+      });
+    }
+  };
 
   render() {
     return (
-      <ValidatorForm ref="form" className="pt-2" onSubmit={this.submitCustomer}>
-        <Grid container className="pt-2" spacing={3}>
-          <Grid item lg={12} xs={12} sm={12} md={12}>
-            <Typography variant="h2">Customer Manage</Typography>
+      // <Fragment>
+      <>
+        <ValidatorForm
+          ref="form"
+          className="pt-2"
+          onSubmit={this.submitCustomer}
+        >
+          <Grid container className="pt-2" spacing={3}>
+            <Grid item lg={12} xs={12} sm={12} md={12}>
+              <Typography variant="h2">Customer Manage</Typography>
+            </Grid>
+            <Grid item xs={12} sm={12} md={6} lg={6}>
+              <Typography variant="subtitle1">Customer Id</Typography>
+              <TextValidator
+                id="outlinedbasic"
+                placeholder="Customer Id"
+                variant="outlined"
+                size="small"
+                style={{ width: "100%" }}
+                value={this.state.formData.id}
+                onChange={(e) => {
+                  let formData = this.state.formData;
+                  formData.id = e.target.value;
+                  this.setState({ formData });
+                }}
+                validators={["required"]}
+              />
+            </Grid>
+            <Grid item xs={12} sm={12} md={6} lg={6}>
+              <Typography variant="subtitle1">Customer Name</Typography>
+              <TextValidator
+                id="outlinedbasic"
+                placeholder="Customer Name"
+                variant="outlined"
+                size="small"
+                style={{ width: "100%" }}
+                value={this.state.formData.name}
+                onChange={(e) => {
+                  let formData = this.state.formData;
+                  formData.name = e.target.value;
+                  this.setState({ formData });
+                }}
+                validators={["required"]}
+              />
+            </Grid>
+            <Grid item xs={12} sm={12} md={6} lg={6}>
+              <Typography variant="subtitle1">Customer Address</Typography>
+              <TextValidator
+                id="outlinedbasic"
+                placeholder="Customer Address"
+                variant="outlined"
+                size="small"
+                style={{ width: "100%" }}
+                value={this.state.formData.address}
+                onChange={(e) => {
+                  let formData = this.state.formData;
+                  formData.address = e.target.value;
+                  this.setState({ formData });
+                }}
+                validators={["required"]}
+              />
+            </Grid>
+            <Grid item xs={12} sm={12} md={6} lg={6}>
+              <Typography variant="subtitle1">Customer Salary</Typography>
+              <TextValidator
+                id="outlinedbasic"
+                placeholder="Customer Salary"
+                variant="outlined"
+                size="small"
+                style={{ width: "100%" }}
+                value={this.state.formData.salary}
+                onChange={(e) => {
+                  let formData = this.state.formData;
+                  formData.salary = e.target.value;
+                  this.setState({ formData });
+                }}
+                validators={["required"]}
+              />
+            </Grid>
+            <Grid
+              container
+              style={{ marginTop: "10px" }}
+              direction="row"
+              justifyContent="flex-end"
+              alignItems="center"
+            >
+              <GDSEButton
+                label="save"
+                type="submit"
+                size="small"
+                color="primary"
+                variant="outlined"
+              />
+            </Grid>
           </Grid>
-          <Grid item xs={12} sm={12} md={6} lg={6}>
-            <Typography variant="subtitle1">Customer Id</Typography>
-            <TextValidator
-              id="outlinedbasic"
-              placeholder="Customer Id"
-              variant="outlined"
-              size="small"
-              style={{ width: "100%" }}
-              // value={}
-              // onChange={(e) => {
-              //     let formData = this.state.formData
-              //     formData.id = e.target.value
-              //     this.setState({ formData })
-              // }}
-              validators={["required"]}
-            />
-          </Grid>
-          <Grid item xs={12} sm={12} md={6} lg={6}>
-            <Typography variant="subtitle1">Customer Name</Typography>
-            <TextValidator
-              id="outlinedbasic"
-              placeholder="Customer Name"
-              variant="outlined"
-              size="small"
-              style={{ width: "100%" }}
-              // value={}
-              // onChange={(e) => {
-              //     let formData = this.state.formData
-              //     formData.id = e.target.value
-              //     this.setState({ formData })
-              // }}
-              validators={["required"]}
-            />
-          </Grid>
-          <Grid item xs={12} sm={12} md={6} lg={6}>
-            <Typography variant="subtitle1">Customer Address</Typography>
-            <TextValidator
-              id="outlinedbasic"
-              placeholder="Customer Address"
-              variant="outlined"
-              size="small"
-              style={{ width: "100%" }}
-              // value={}
-              // onChange={(e) => {
-              //     let formData = this.state.formData
-              //     formData.id = e.target.value
-              //     this.setState({ formData })
-              // }}
-              validators={["required"]}
-            />
-          </Grid>
-          <Grid item xs={12} sm={12} md={6} lg={6}>
-            <Typography variant="subtitle1">Customer Salary</Typography>
-            <TextValidator
-              id="outlinedbasic"
-              placeholder="Customer Salary"
-              variant="outlined"
-              size="small"
-              style={{ width: "100%" }}
-              // value={}
-              // onChange={(e) => {
-              //     let formData = this.state.formData
-              //     formData.id = e.target.value
-              //     this.setState({ formData })
-              // }}
-              validators={["required"]}
-            />
-          </Grid>
-          <Grid
-            container
-            style={{ marginTop: "10px" }}
-            direction="row"
-            justifyContent="flex-end"
-            alignItems="center"
-          >
-            <GDSEButton
-              label="save"
-              type="submit"
-              size="small"
-              color="primary"
-              variant="outlined"
-            />
-          </Grid>
-        </Grid>
-      </ValidatorForm>
+        </ValidatorForm>
+        <GDSESnackBar
+          open={this.state.alert}
+          onClose={() => {
+            this.setState({ alert: false });
+          }}
+          message={this.state.message}
+          autoHideDuration={3000}
+          severity={this.state.severity}
+          variant="filled"
+        />
+      </>
+      // </Fragment>
     );
   }
 }
